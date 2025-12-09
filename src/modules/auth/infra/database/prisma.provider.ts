2fd7a01298d56b5ce013808f6e377bd8a19ed4b6
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2025-12-09 12:51:31 -0300

    feat: implementar serviço Prisma para gerenciamento de conexão com o banco de dados

---
+++ src/modules/auth/infra/database/prisma.provider.ts
@@ -0,0 +1,46 @@
+import {
+  OnModuleDestroy,
+  OnModuleInit,
+  Injectable,
+  Inject,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import { Env } from '@/env';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor(@Inject(ConfigService) configService: ConfigService<Env, true>) {
+    const databaseUrl = configService.get('DATABASE_URL', { infer: true });
+    const nodeEnv = configService.get('NODE_ENV', { infer: true });
+
+    const pool = new Pool({ connectionString: databaseUrl });
+    const adapter = new PrismaPg(pool);
+
+    super({
+      adapter,
+      log:
+        nodeEnv === 'development'
+          ? ['query', 'info', 'warn', 'error']
+          : ['warn', 'error'],
+    });
+  }
+
+  onModuleDestroy() {
+    this.logger.log('Disconnecting from database');
+    void this.$disconnect();
+  }
+
+  onModuleInit() {
+    this.logger.log('Connecting to database');
+    void this.$connect();
+  }
+}
