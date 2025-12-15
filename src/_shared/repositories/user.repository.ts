7ffed352482efe99f895b363201aea0cc0bb1ecf
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2025-12-15 10:09:16 -0300

    feat: criar repositório abstrato para gerenciamento de usuários

---
+++ src/_shared/repositories/user.repository.ts
@@ -0,0 +1,11 @@
+import { User } from '@/modules/auth/domain/entities/user.entity';
+
+export abstract class UserRepository {
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract findById(id: string): Promise<User | null>;
+  abstract create(
+    name: string,
+    email: string,
+    passwordHash: string,
+  ): Promise<User>;
+}
