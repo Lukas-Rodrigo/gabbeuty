df92060e8528ebb305ee149eff44eebe006e97d8
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2025-12-16 12:10:28 -0300

    feat: adicionar eventos para agendamentos confirmados e cancelados

---
+++ src/modules/gabbeuty-crm/domain/events/appointment-confirmed.event.ts
@@ -0,0 +1,26 @@
+import { DomainEvent } from '@/_shared/event/domain-event';
+import { AppointmentStatus } from '../entities/value-objects/appointment-status.vo';
+import { UniqueEntityID } from '@/_shared/value-objects/unique-entity-id.vo';
+
+export interface AppointmentConfirmedEventPayload {
+  appointmentId: string;
+  userId: string;
+  clientId: string;
+  clientName?: string;
+  clientPhoneNumber: string;
+  date: Date;
+  status: AppointmentStatus;
+  title: string;
+}
+
+export class AppointmentConfirmedEvent implements DomainEvent {
+  public occurredAt: Date;
+
+  constructor(public readonly payload: AppointmentConfirmedEventPayload) {
+    this.occurredAt = new Date();
+  }
+
+  getAggregateId(): UniqueEntityID {
+    return new UniqueEntityID(this.payload.appointmentId);
+  }
+}
