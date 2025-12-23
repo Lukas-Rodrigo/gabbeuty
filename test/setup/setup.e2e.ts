7a8ac9685bac68dd06d530c7b4b0baa0b9b92c85
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2025-12-23 08:55:07 -0300

    refactor: downgrade `Prisma client` version e remocao de adapter

--- test/setup/setup.e2e.ts
+++ test/setup/setup.e2e.ts
@@ -2,14 +2,12 @@ import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import { execSync } from 'node:child_process';
 import { randomUUID } from 'node:crypto';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
-let prisma: PrismaClient;
+let prisma = new PrismaClient();
 
 function generateUniqueDataBaseUrl(schemaId: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provider a DATABASE_URL environment variable');
+    throw new Error('Please provider a DATABASE_URL enrinment variable');
   }
 
   const url = new URL(process.env.DATABASE_URL);
@@ -24,30 +22,20 @@ beforeAll(async () => {
   const dataBaseUrl = generateUniqueDataBaseUrl(uniqueDataBaseUrl);
   process.env.DATABASE_URL = dataBaseUrl;
 
-  const cleanupPool = new Pool({ connectionString: dataBaseUrl });
-  const cleanupAdapter = new PrismaPg(cleanupPool);
-  const cleanupPrisma = new PrismaClient({
-    adapter: cleanupAdapter,
-  });
+  const cleanupPrisma = new PrismaClient();
   try {
     await cleanupPrisma.$executeRawUnsafe(
       `DROP SCHEMA IF EXISTS "${uniqueDataBaseUrl}" CASCADE`,
     );
   } catch (error) {
     console.log(error);
-    throw error;
   } finally {
     await cleanupPrisma.$disconnect();
-    await cleanupPool.end();
   }
 
   execSync('npx prisma db push --force-reset', { stdio: 'inherit' });
 
-  const pool = new Pool({ connectionString: dataBaseUrl });
-  const adapter = new PrismaPg(pool);
-  prisma = new PrismaClient({
-    adapter,
-  });
+  prisma = new PrismaClient();
 });
 
 afterAll(async () => {
